46fc5fa5f0895efab125f21078ffe308c703bee7
Leonardo <leonardomonteirolima@gmail.com>
2024-04-15 16:40:00 -0300

    Criado a parte de carrer

--- Balta/ContentContext/Career.cs
+++ Balta/ContentContext/Career.cs
@@ -2,6 +2,14 @@ namespace Balta.ContentContext
 {
     public class Career : Content
     {
-        public int Courses { get; set; }
+        public Career()
+        {
+            Items = new List<CareerItem>();
+        }
+
+        public IList<CareerItem> Items { get; set; }
+
+        public int TotalCourses => Items.Count;
+        //Expression Body
     }
 }
